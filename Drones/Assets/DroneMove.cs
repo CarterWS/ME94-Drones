8eb505ec05d5ed8972b2d6f1d233c55dd9c484a2
CarterWS <carter.silvey@tufts.edu>
2019-04-08 23:49:22 -0400

    Setting Up

--- Drones/Assets/DroneMove.cs
+++ Drones/Assets/DroneMove.cs
@@ -5,14 +5,15 @@ using UnityEngine.AI;
 
 public class DroneMove : MonoBehaviour
 {
-    public float droneSpeed;
     public GameObject waypoint;
     public NavMeshAgent drone;
     Vector3 target = new Vector3();
+    Vector3 startPos = new Vector3(0, 0.75f, -1);
+
     // Start is called before the first frame update
     void Start()
     {
-
+        drone.Warp(startPos);
     }
 
     // Update is called once per frame
